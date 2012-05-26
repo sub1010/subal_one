f57282c27c8ced1191463a5986367a4c4f53ca27
Subal Charla <subalcharla@gmail.com>
2012-05-26 09:28:39 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubalOne::Application.config.secret_token = '2e87165c072928bc2737b859a0faaad05e226d69f3a716403d453b12b21726363cbb039efb839959ccacbb115ccd58e662c25cde4d9415bfce1954640dd2cb40'
